41660cd9abac1d8609371eb8e1bbe33f26b86f4b
Paritosh Jha <paritoshjhaparitoshjha0504@gmail.com>
2024-05-19 09:45:48 +0530

    Added get route for redirection

--- routes/url.js
+++ routes/url.js
@@ -1,8 +1,25 @@
 import express from 'express'
-import {handleGenerateNewShortUrl} from '../controllers/url.js'
+import { handleGenerateNewShortUrl } from '../controllers/url.js'
+import URL from '../models/url.js'
 
 const router = express.Router()
 
 router.post('/', handleGenerateNewShortUrl)
+
+router.get('/:id', async (req, res) => {
+  const shortId = req.params.id
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId: shortId,
+    },
+    {
+      $push: {
+        visitHistory: { timestamp: Date.now() },
+      },
+    }
+  )
+
+  res.redirect(entry.redirectUrl)
+})
 
 export default router
